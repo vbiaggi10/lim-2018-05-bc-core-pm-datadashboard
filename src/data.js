83e12fe80d40e2f9e2bc74da61a31cae62cc6471
vbiaggi10 <vbiaggi10@gmail.com>
2018-06-21 18:41:15 -0500

    combinando api

--- src/data.js
+++ src/data.js
@@ -1,31 +1,36 @@
-const userData = document.querySelector('.studentsContainer');
-fetch('http://127.0.0.1:5500/data/cohorts/lim-2018-03-pre-core-pw/users.json')
-    .then(function (responseUsers) {
-        return responseUsers.json();
-    })
-    .then(function (dataUser) {
-        functionUser(dataUser);
-    });
-function functionUser(intento) {
-    let tamaño = intento.length;
-    for (let i = 0; i < tamaño; i++) {
-        let myName = document.createElement('h1');
-        let myID = document.createElement('p');
-        let myRole = document.createElement('p');
-        let container = document.createElement('div');
-        let photo = document.createElement('img');
-        let img = 'img/woman.png';
-        photo.setAttribute('src',img);
-        container.classList.add('container');
-        myName.textContent = intento[i].name;
-        myID.textContent = 'ID: ' + intento[i].id;
-        myRole.textContent = 'Role: ' + intento[i].role;
-        container.appendChild(photo);
-        container.appendChild(myName);
-        container.appendChild(myID);
-        container.appendChild(myRole);
-        
-        userData.appendChild(container);
+/* const userData = document.querySelector('.studentsContainer');
+const hola = document.querySelector('.venuesContainer');
+const subtittle = document.querySelector('#subtittleContainer');
+fetch('https://api.laboratoria.la/cohorts')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    functionCohort(data);
+  })
+hola.addEventListener("change", (event) => {
+  if (hola.value === "lim-2018-03-pre-core-pw") {
+    let mysubtittle = document.createElement('h1');
+    mysubtittle.textContent="lim-2018-03-pre-core-pw";
+    subtittle.appendChild(mysubtittle);
+    fetch('https://api.laboratoria.la/cohorts/lim-2018-03-pre-core-pw/users')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        functionUser(data);
+      })
+  } else if (hola.value !== "lim-2018-03-pre-core-pw") {
+    alert("Aún no hay datos") ;
+  }
 
-    }
+}) 
+
+function functionCohort(intento){
+  for (let i = 0; i < intento.length; i++) {
+    
+    hola.innerHTML += "<option value=" + intento[i].id + " >" + intento[i].id + "</option>";
+    
+  }
 }
+ */
--- src/main.js
+++ src/main.js
@@ -1,9 +1,86 @@
+var apiRequest0 = fetch('https://api.laboratoria.la/campuses/').then(function (response) {
+  return response.json()
+});
+
+var apiRequest1 = fetch('https://api.laboratoria.la/cohorts/').then(function (response) {
+  return response.json()
+});
+
+var apiRequest2 = fetch('https://api.laboratoria.la/cohorts/lim-2018-03-pre-core-pw/users/').then(function (response) {
+  return response.json()
+});
+
+var apiRequest3 = fetch('https://api.laboratoria.la/cohorts/lim-2018-03-pre-core-pw/progress/').then(function (response) {
+  return response.json()
+});
+
+var combinedData = { "apiRequest0": {}, "apiRequest1": {}, "apiRequest2": {}, "apiRequest3": {} };
+Promise.all([apiRequest0, apiRequest1, apiRequest2, apiRequest3]).then(function (values) {
+  combinedData["apiRequest0"] = values[0]
+  combinedData["apiRequest1"] = values[1];
+  combinedData["apiRequest2"] = values[2];
+  combinedData["apiRequest3"] = values[3];
+  const listCampuses = document.querySelector('#listCampuses');
+  const listCohorts = document.querySelector('.venuesContainer');
+  const listUsers = document.querySelector('#listUsers');
+  for (let i = 0; i < values[1].length; i++) {
+    const optionElements = document.createElement('option');
+    const contenidoOption = document.createTextNode(values[1][i].id);
+    optionElements.appendChild(contenidoOption);
+    listCohorts.appendChild(optionElements);
+  }
+  listCohorts.addEventListener('change', () => {
+    if (listCohorts.value === 'lim-2018-03-pre-core-pw') {
+      const mysubtittle = document.createElement('h1');
+      mysubtittle.textContent = "lim-2018-03-pre-core-pw";
+      subtittle.appendChild(mysubtittle);
+      functionUser(values[2], values[3]);
+    }else{
+      alert("Aún no hay datos") ;
+    }
+  })
+  //Listando a los usuarios
+
+  return combinedData;
+});
+
+function functionUser(users, progress) {
+  let tamaño = users.length;
+  for (let i = 0; i < tamaño; i++) {
+    let myName = document.createElement('h1');
+    let myID = document.createElement('p');
+    let myRole = document.createElement('p');
+    let myProgress = document.createElement('p');
+    let container = document.createElement('div');
+    let photo = document.createElement('img');
+    let img = 'img/woman.png';
+    photo.setAttribute('src', img);
+    container.classList.add('container');
+    myName.textContent = users[i].name;
+    myID.textContent = 'ID: ' + users[i].id;
+    myRole.textContent = 'Role: ' + users[i].role;
+    myProgress.textContent = 'Progress: ' + progress[i].intro.percent;
+    container.appendChild(photo);
+    container.appendChild(myName);
+    container.appendChild(myID);
+    container.appendChild(myRole);
+    container.appendChild(myProgress);
+
+    userData.appendChild(container);
+
+  }
+}
+
+/* window.computeUsersStats = (users, progress, courses) => {
+    const listOfUser = users.map();
+
+} */
+
+
+
 let aux1 = 1;
-let aux2 = 1;
-let choiceClassVenue = document.querySelectorAll('.venueContainer');
+let choiceClassVenue = document.querySelectorAll('.venuesContainer');
 let venuesContainer = document.querySelector('#venuesContainer');
-let choiceClassYear = document.querySelectorAll('.yearContainer');
-let yearsContainer = document.querySelector('#yearsContainer');
 venuesContainer.addEventListener('click', () => {
 
   if (aux1 === 1) {
@@ -18,20 +95,5 @@ venuesContainer.addEventListener('click', () => {
     }
     venuesContainer.style.background = '#ffffff';
     aux1 = 1;
-  }
-});
-yearsContainer.addEventListener('click', () => {
-  if (aux2 === 1) {
-    for (let newStyle of choiceClassYear) {
-      newStyle.style.display = 'flex';
-    }
-    yearsContainer.style.background = '#56f89a';
-    aux2 = 0;
-  } else {
-    for (let newStyle of choiceClassYear) {
-      newStyle.style.display = 'none';
-    }
-    yearsContainer.style.background = '#ffffff';
-    aux2 = 1;
   }
 });
