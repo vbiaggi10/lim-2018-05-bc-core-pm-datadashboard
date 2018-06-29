bc8318b12af8cf12cf610372e3a528c2bd2c3391
vbiaggi10 <vbiaggi10@gmail.com>
2018-06-29 17:58:19 -0500

    left navbar

--- src/main.js
+++ src/main.js
@@ -5,40 +5,20 @@ processing = () => {
 }
 window.onload = () => { processing() }
 
-let aux1 = 1;
-let aux2 = 1;
-let choiceClassVenue = document.querySelectorAll('.venueContainer');
-let venuesContainer = document.querySelector('#venuesContainer');
-let choiceClassYear = document.querySelectorAll('.yearContainer');
-let yearsContainer = document.querySelector('#yearsContainer');
-venuesContainer.addEventListener('click', () => {
+// MENU RESPONSIVE
+let auxMenuResponsive = 1;
+document.querySelector('.menu').addEventListener('click', () => {
 
-  if (aux1 === 1) {
-    for (let newStyle of choiceClassVenue) {
-      newStyle.style.display = 'flex';
-    }
-    venuesContainer.style.background = '#ffe521';
-    aux1 = 0;
+  let openLeftNavBar = document.querySelector('.left-nav-bar');
+  let openColumn = document.querySelector('.column');
+
+  if (auxMenuResponsive === 1) {
+    openLeftNavBar.style.display = 'block';
+    openColumn.style.height = '100vh';
+    auxMenuResponsive = 0;
   } else {
-    for (let newStyle of choiceClassVenue) {
-      newStyle.style.display = 'none';
-    }
-    venuesContainer.style.background = '#ffffff';
-    aux1 = 1;
-  }
-});
-yearsContainer.addEventListener('click', () => {
-  if (aux2 === 1) {
-    for (let newStyle of choiceClassYear) {
-      newStyle.style.display = 'flex';
-    }
-    yearsContainer.style.background = '#56f89a';
-    aux2 = 0;
-  } else {
-    for (let newStyle of choiceClassYear) {
-      newStyle.style.display = 'none';
-    }
-    yearsContainer.style.background = '#ffffff';
-    aux2 = 1;
+    openLeftNavBar.style.display = 'none';
+    openColumn.style.height = '65px';
+    auxMenuResponsive = 1;
   }
 });
