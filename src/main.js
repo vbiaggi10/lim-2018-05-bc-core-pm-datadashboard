6feafc985ee3efbe1768acf02875ec9f3158eca9
vbiaggi10 <vbiaggi10@gmail.com>
2018-06-29 18:59:49 -0500

    layout with basic functionalities

--- src/main.js
+++ src/main.js
@@ -22,3 +22,50 @@ document.querySelector('.menu').addEventListener('click', () => {
     auxMenuResponsive = 1;
   }
 });
+
+//SIGN OFF
+document.querySelector('#sign-off').addEventListener('click', (e) => {
+  e.preventDefault();
+  window.location.href = 'login.html';
+})
+
+// DISPLAY SORT
+let auxSort = 1;
+document.querySelector('#sort-img').addEventListener('click', () => {
+
+  let openSortContainer = document.querySelector('#open-sort-container');
+  let scrollInfo = document.querySelector('.information-container');
+  // margin-top: 300px;
+  if (auxSort === 1) {
+    console.log('open')
+    openSortContainer.style.display = 'block';
+    scrollInfo.style = 'margin-top: 200px';
+    auxSort = 0;
+  } else {
+    console.log('close')
+    openSortContainer.style.display = 'none';
+    scrollInfo.style = 'margin-top: 100px';
+    auxSort = 1;
+  }
+});
+
+//Change style of campuses-container
+let aux1 = 1;
+const campusesContainer = document.querySelector('#campuses-container');
+campusesContainer.addEventListener('click', () => {
+
+  let choiceClassVenue = document.querySelectorAll('.campuses-container');
+  if (aux1 === 1) {
+    for (let newStyle of choiceClassVenue) {
+      newStyle.style.display = 'flex';
+    }
+    campusesContainer.style.background = '#ffe521';
+    aux1 = 0;
+  } else {
+    for (let newStyle of choiceClassVenue) {
+      newStyle.style.display = 'none';
+    }
+    campusesContainer.style.background = '#ffffff';
+    aux1 = 1;
+  }
+});
