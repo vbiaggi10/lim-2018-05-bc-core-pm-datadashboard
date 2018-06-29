10951e3766fee789d83cc91784c285a29f366b24
vbiaggi10 <vbiaggi10@gmail.com>
2018-06-29 14:35:45 -0500

    login js linked index

---
+++ src/login.js
@@ -0,0 +1,12 @@
+document.querySelector('#submit').addEventListener('click', (e) => {
+  e.preventDefault();
+  let userEmail = document.querySelector('#email').value;
+  let userPassword = document.querySelector('#password').value;
+  if (userEmail === 'admin' && userPassword === 'admin') {
+
+    window.location.href = 'index.html';
+
+  } else {
+    alert('No es el usuario o contraseña correcta');
+  }
+})
